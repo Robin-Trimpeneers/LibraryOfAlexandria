4b8c48b58c67f9e5f587efeef035f39b5187d78c
RobinTrimpeneersPXL <148947741+RobinTrimpeneersPXL@users.noreply.github.com>
2025-10-06 09:58:49 +0200

    fix

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,8 +1,8 @@
 // config.js
 const config = {
     API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
-        ? 'http://localhost:8080/api' 
-        : `${window.location.protocol}//${window.location.host}/api`,
+        ? 'http://localhost:8080' 
+        : `${window.location.protocol}//${window.location.host}`,
     AUTH_COOKIE_NAME: 'JWT',
     DEFAULT_BOOK_COVER: '/assets/default-book-cover.jpg',
     NOTIFICATION_DURATION: 5000,
